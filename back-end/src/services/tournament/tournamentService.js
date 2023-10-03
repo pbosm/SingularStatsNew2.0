8ef0d1401301f6ea8fd58d6f30fe67fc035133b8
Pablo <pablo.sm14@aluno.ifsc.edu.br>
2023-10-02 23:14:44 -0300

    Otimizando function getTournament

--- back-end/src/services/tournament/tournamentService.js
+++ back-end/src/services/tournament/tournamentService.js
@@ -16,124 +16,56 @@ class TournamentService {
                 'LPL': 'summer',
                 'CBLOL': 'split 2'
             };
-            
-            const resultCblol = await Cblol.findAll({
-                attributes: [
-                    'league',
-                    'split',
-                    [db.fn('sum', db.literal("result=1")), 'games'],
-                    [
-                        db.literal(`sum(duracaogame) / 2 / sum(result=1) / 60`),
-                        'game_duration'
+
+            const leagues = [
+                { league: 'CBLOL', model: Cblol, splitName: split['CBLOL'] },
+                { league: 'LCK',   model: Lck,   splitName: split['LCK'] },
+                { league: 'LPL',   model: Lpl,   splitName: split['LPL'] }
+            ];
+
+            const queries = leagues.map(async ({ league, model, splitName }) => {
+                const result = await model.findAll({
+                    attributes: [
+                        'league',
+                        'split',
+                        [db.fn('sum', db.literal("result=1")), 'games'],
+                        [
+                            db.literal(`sum(duracaogame) / 2 / sum(result=1) / 60`),
+                            'game_duration'
+                        ],
+                        [
+                            db.literal(`sum(teamkills) / sum(result=1)`),
+                            'average_kill'
+                        ],
+                        [
+                            db.literal(`sum(result=1 and side='blue') / sum(result=1) * 100`),
+                            'win_rate_blue'
+                        ],
+                        [
+                            db.literal(`sum(result=1 and side='red') / sum(result=1) * 100`),
+                            'win_rate_red'
+                        ],
+                        [
+                            db.literal(`sum(dragons) / sum(result=1)`),
+                            'average_dragons'
+                        ],
+                        [
+                            db.literal(`sum(barons) / sum(result=1)`),
+                            'average_barons'
+                        ],
                     ],
-                    [
-                        db.literal(`sum(teamkills) / sum(result=1)`),
-                        'average_kill'
-                    ],
-                    [
-                        db.literal(`sum(result=1 and side='blue') / sum(result=1) * 100`),
-                        'win_rate_blue'
-                    ],
-                    [
-                        db.literal(`sum(result=1 and side='red') / sum(result=1) * 100`),
-                        'win_rate_red'
-                    ],
-                    [
-                        db.literal(`sum(dragons) / sum(result=1)`),
-                        'average_dragons'
-                    ],
-                    [
-                        db.literal(`sum(barons) / sum(result=1)`),
-                        'average_barons'
-                    ],
-                ],
-                where: {
-                    split: split['CBLOL'],
-                    league: 'CBLOL',
-                    position: 'team',
-                },
-                group: ['league'],
+                    where: {
+                        split: splitName,
+                        league: league,
+                        position: 'team',
+                    },
+                    group: ['league'],
+                });
+                return result;
             });
+            const result = await Promise.all(queries);
 
-            const resultLck = await Lck.findAll({
-                attributes: [
-                    'league',
-                    'split',
-                    [db.fn('sum', db.literal("result=1")), 'games'],
-                    [
-                        db.literal(`sum(duracaogame) / 2 / sum(result=1) / 60`),
-                        'game_duration'
-                    ],
-                    [
-                        db.literal(`sum(teamkills) / sum(result=1)`),
-                        'average_kill'
-                    ],
-                    [
-                        db.literal(`sum(result=1 and side='blue') / sum(result=1) * 100`),
-                        'win_rate_blue'
-                    ],
-                    [
-                        db.literal(`sum(result=1 and side='red') / sum(result=1) * 100`),
-                        'win_rate_red'
-                    ],
-                    [
-                        db.literal(`sum(dragons) / sum(result=1)`),
-                        'average_dragons'
-                    ],
-                    [
-                        db.literal(`sum(barons) / sum(result=1)`),
-                        'average_barons'
-                    ],
-                ],
-                where: {
-                    split: split['LCK'],
-                    league: 'LCK',
-                    position: 'team',
-                },
-                group: ['league'],
-            });
-
-            const resultLpl = await Lpl.findAll({
-                attributes: [
-                    'league',
-                    'split',
-                    [db.fn('sum', db.literal("result=1")), 'games'],
-                    [
-                        db.literal(`sum(duracaogame) / 2 / sum(result=1) / 60`),
-                        'game_duration'
-                    ],
-                    [
-                        db.literal(`sum(teamkills) / sum(result=1)`),
-                        'average_kill'
-                    ],
-                    [
-                        db.literal(`sum(result=1 and side='blue') / sum(result=1) * 100`),
-                        'win_rate_blue'
-                    ],
-                    [
-                        db.literal(`sum(result=1 and side='red') / sum(result=1) * 100`),
-                        'win_rate_red'
-                    ],
-                    [
-                        db.literal(`sum(dragons) / sum(result=1)`),
-                        'average_dragons'
-                    ],
-                    [
-                        db.literal(`sum(barons) / sum(result=1)`),
-                        'average_barons'
-                    ],
-                ],
-                where: {
-                    split: split['LPL'],
-                    league: 'LPL',
-                    position: 'team',
-                },
-                group: ['league'],
-            });
-
-            const result = [...resultCblol, ...resultLck, ...resultLpl];
-
-            return result;
+            return result.flat();
         } catch (error) {
             throw new Error(`Erro ao buscar os dados. ${error.message}`);
         }
